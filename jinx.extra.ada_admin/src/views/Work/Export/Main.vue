3d87473b3031c3820bdae6dd2980f74665e9da1c
jinxin201211 <jinxin19950105@outlook.com>
2022-06-14 17:28:49 +0800

    大广赛获奖作品导出

--- jinx.extra.ada_admin/src/views/Work/Export/Main.vue
+++ jinx.extra.ada_admin/src/views/Work/Export/Main.vue
@@ -34,13 +34,13 @@
           <el-table-column prop="works_type" label="作品类别"> </el-table-column>
           <el-table-column prop="works_series_name" label="命题名称"> </el-table-column>
           <el-table-column prop="wno" label="参赛编号"> </el-table-column>
-          <!--<el-table-column prop="works_name" label="作品名称"> </el-table-column>-->
+          <el-table-column prop="works_name" label="作品名称"> </el-table-column>
           <el-table-column prop="author1" label="作者"> </el-table-column>
           <el-table-column prop="tel" label="作者电话"> </el-table-column>
           <el-table-column prop="t_uname" label="指导教师"> </el-table-column>
           <el-table-column prop="t_tel" label="教师电话"> </el-table-column>
           <el-table-column prop="school" label="学校"> </el-table-column>
-          <!-- <el-table-column prop="department" label="院系"> </el-table-column> -->
+          <el-table-column prop="department" label="院系"> </el-table-column>
           <!-- <el-table-column prop="other" label="其他"> </el-table-column> -->
           <el-table-column prop="major" label="专业"> </el-table-column>
           <el-table-column prop="prize" label="获奖等级"> </el-table-column>
@@ -180,8 +180,8 @@ export default {
       this.getExcel(head, filter, this.TableDataPrizeStat, "赛区报送全国总评审作品数量统计表");
     },
     handleExportPrize() {
-      let head = ["序号", "作品类别", "命题名称", "参赛编号", "作者", "作者电话", "指导教师", "教师电话", "学校", "专业", "获奖等级"];
-      let filter = ["seq", "works_type", "works_series_name", "wno", "author1", "tel", "t_uname", "t_tel", "school", "major", "prize"];
+      let head = ["序号", "作品类别", "命题名称", "参赛编号", "作品名称", "作者", "作者电话", "指导教师", "教师电话", "学校", "院系", "专业", "获奖等级"];
+      let filter = ["seq", "works_type", "works_series_name", "wno", "works_name", "author1", "tel", "t_uname", "t_tel", "school", "department", "major", "prize"];
       this.getExcel(head, filter, this.TableDataPrize, "赛区获奖名单");
     },
     handleExportPrizeAttachment() {
